8d6d09146b3ea69c5c4e8a33fd9d3c9e2cc66a79
Lan <lanpham312@gmail.com>
2018-05-23 11:17:54 +0100

    adding youtube search response

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'penguins'}, (videos) => {
+      this.setState({ videos });
+      //this.setState({ videos: videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
-ReactDom.render(<App />, document.querySelector('.container'))
+ReactDom.render(<App />, document.querySelector('.container'));
