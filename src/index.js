1eb8e42127c18f37bbd45db2076690f6eae45a2b
Lan <lanpham312@gmail.com>
2018-05-25 16:23:05 +0100

    updating search bar to have search terms

--- src/index.js
+++ src/index.js
@@ -14,7 +14,11 @@ class App extends Component {
       selectedVideo: null
     };
 
-    YTSearch({key: ENV.YOUTUBE_API_KEY, term: 'the libertines'}, (videos) => {
+    this.videoSearch('the libertines');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: ENV.YOUTUBE_API_KEY, term: term}, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
@@ -25,7 +29,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
